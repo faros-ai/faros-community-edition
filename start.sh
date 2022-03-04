5bbd5abb031b301cb6a19f31de3dfc50afd1fc50
Yandry Perez Clemente <99700024+ypc-faros@users.noreply.github.com>
2022-03-04 13:21:58 -0500

    Use metabase image built for M1

--- start.sh
+++ start.sh
@@ -25,8 +25,8 @@ fi
 export FAROS_EMAIL=$EMAIL
 
 if [[ `uname -m 2> /dev/null` == 'arm64' ]]; then
-    # Use Airbyte images built for Apple M1
-    AIRBYTE_IMAGE_PREFIX="farosai/airbyte-" docker-compose up --build --remove-orphans
+    # Use Airbyte and Metabase images built for Apple M1
+    AIRBYTE_IMAGE_PREFIX="farosai/airbyte-" METABASE_IMAGE="farosai/metabase-m1" docker-compose up --build --remove-orphans
 else
     docker-compose up --build --remove-orphans
 fi

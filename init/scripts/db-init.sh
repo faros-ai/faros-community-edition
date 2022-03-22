3532809ee0747e18942f82bacdb0f99556c2e6b5
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2022-03-22 11:49:17 -0400

    Makes it possible to use different databases (#74)
    Co-authored-by: Matthew Tovbin <tovbinm@users.noreply.github.com>

--- init/scripts/db-init.sh
+++ init/scripts/db-init.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 
-db_host=${FAROS_DB_HOST}
-db_port=${FAROS_DB_PORT}
-db_user=${FAROS_DB_USER}
-db_pass=${FAROS_DB_PASSWORD}
 faros_db_name=${FAROS_DB_NAME}
+faros_db_host=${FAROS_DB_HOST}
+faros_db_port=${FAROS_DB_PORT}
+faros_db_user=${FAROS_DB_USER}
+faros_db_pass=${FAROS_DB_PASSWORD}
+
+cfg_db_host=${FAROS_CONFIG_DB_HOST}
+cfg_db_port=${FAROS_CONFIG_DB_PORT}
+cfg_db_user=${FAROS_CONFIG_DB_USER}
+cfg_db_pass=${FAROS_CONFIG_DB_PASSWORD}
+
 hasura_db_name=${HASURA_DB_NAME}
 metabase_db_name=${METABASE_DB_NAME}
 n8n_db_name=${N8N_DB_NAME}
 
-db_url="postgres://$db_user:$db_pass@$db_host:$db_port"
-
 create_database() {
   db_name=$1
+  db_host=$2
+  db_port=$3
+  db_user=$4
+  db_pass=$5
+  db_url="postgres://$db_user:$db_pass@$db_host:$db_port"
+
   if [ "$( psql "$db_url" -tAc "SELECT 1 FROM pg_database WHERE datname='$db_name'" )" = '1' ]
   then
       echo "Database $db_name already exists"
@@ -22,16 +32,16 @@ create_database() {
   fi
 }
 
-create_database "$faros_db_name"
+create_database "$faros_db_name" "$faros_db_host" "$faros_db_port" "$faros_db_user" "$faros_db_pass"
 
 echo "Applying Flyway migrations"
 flyway \
   -locations="filesystem:/home/faros/canonical-schema" \
-  -url="jdbc:postgresql://$db_host:$db_port/$faros_db_name" \
-  -user="$db_user" \
-  -password="$db_pass" \
+  -url="jdbc:postgresql://$faros_db_host:$faros_db_port/$faros_db_name" \
+  -user="$faros_db_user" \
+  -password="$faros_db_pass" \
   migrate
 
-create_database "$hasura_db_name"
-create_database "$metabase_db_name"
-create_database "$n8n_db_name"
+create_database "$hasura_db_name" "$cfg_db_host" "$cfg_db_port" "$cfg_db_user" "$cfg_db_pass"
+create_database "$metabase_db_name" "$cfg_db_host" "$cfg_db_port" "$cfg_db_user" "$cfg_db_pass"
+create_database "$n8n_db_name" "$cfg_db_host" "$cfg_db_port" "$cfg_db_user" "$cfg_db_pass"

3a8e786c6e231449c78619268ae78a51897d4a1a
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2022-03-30 01:25:09 -0400

    Makes it possible to initialize a cloud deployment using docker-compose (#85)
    * Makes it possible to initialize a cloud deployment using docker-compose
    
    * Fix shellcheck issues
    
    * Reuse HASURA_GRAPHQL_DATABASE_URL
    
    * Fix cmd line optional args building

--- init/scripts/entrypoint.sh
+++ init/scripts/entrypoint.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 airbyte_url=$AIRBYTE_URL
 airbyte_force_setup=$AIRBYTE_FORCE_SETUP
+airbyte_api_calls_concurrency=$AIRBYTE_API_CALLS_CONCURRENCY
+hasura_admin_secret=$HASURA_GRAPHQL_ADMIN_SECRET
+hasura_database_url=$HASURA_GRAPHQL_DATABASE_URL
 hasura_url=$HASURA_URL
 metabase_url=$METABASE_URL
 db_host=$FAROS_DB_HOST
@@ -9,13 +12,25 @@ db_port=$FAROS_DB_PORT
 
 ./wait-for/wait-for.sh "$db_host":"$db_port" -- ./db-init.sh
 
+airbyte_optional_args=()
+
 if [ "${airbyte_force_setup}" = "true" ]; then
-    ./wait-for/wait-for.sh "$airbyte_url"/api/v1/health -t 60 -- node ../lib/airbyte/init --airbyte-url "$airbyte_url" --force-setup
-else
-    ./wait-for/wait-for.sh "$airbyte_url"/api/v1/health -t 60 -- node ../lib/airbyte/init --airbyte-url "$airbyte_url"
+    airbyte_optional_args=(--force-setup)
 fi
 
-./wait-for/wait-for.sh "$hasura_url"/healthz -t 60 -- node ../lib/hasura/init --hasura-url "$hasura_url"
+if [ -n "${airbyte_api_calls_concurrency}" ]; then
+    airbyte_optional_args=("${airbyte_optional_args[@]}" --airbyte-api-calls-concurrency "${airbyte_api_calls_concurrency}")
+fi
+
+./wait-for/wait-for.sh "$airbyte_url"/api/v1/health -t 60 -- node ../lib/airbyte/init --airbyte-url "$airbyte_url" "${airbyte_optional_args[@]}"
+
+hasura_optional_args=()
+
+if [ -n "${hasura_database_url}" ]; then
+    hasura_optional_args=("${hasura_optional_args[@]}" --database-url "${hasura_database_url}")
+fi
+
+./wait-for/wait-for.sh "$hasura_url"/healthz -t 60 -- node ../lib/hasura/init --hasura-url "$hasura_url" --admin-secret "$hasura_admin_secret" "${hasura_optional_args[@]}"
 ./wait-for/wait-for.sh "$metabase_url"/api/health -t 60 -- ./metabase-init.sh
 
 node ../lib/banner

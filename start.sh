c13bb52033add9be9970123af100d2c282be32cf
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2022-05-04 10:36:30 -0400

    Adds scarf prefix to init and airbyte/metabase m1 images (#126)

--- start.sh
+++ start.sh
@@ -29,11 +29,13 @@ fi
 export FAROS_EMAIL=$EMAIL
 
 # Ensure we're using the latest faros-init image
+FAROS_INIT_IMAGE=farosai.docker.scarf.sh/farosai/faros-ce-init:latest \
 docker-compose pull faros-init
 
 if [[ $(uname -m 2> /dev/null) == 'arm64' ]]; then
     # Use Airbyte and Metabase images built for Apple M1
-    AIRBYTE_IMAGE_PREFIX="farosai/airbyte-" METABASE_IMAGE="farosai/metabase-m1" docker-compose up --build --remove-orphans
+    AIRBYTE_IMAGE_PREFIX="farosai.docker.scarf.sh/farosai/airbyte-" METABASE_IMAGE="farosai.docker.scarf.sh/farosai/metabase-m1" \
+    docker-compose up --build --remove-orphans
 else
     docker-compose up --build --remove-orphans
 fi

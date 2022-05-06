0fc3edbd370530c1c44e369517fe010e3d00d86e
Yandry Perez Clemente <99700024+ypc-faros@users.noreply.github.com>
2022-05-06 19:39:32 -0400

    rm dbt target

--- dbt-transforms/target/compiled/faros_community_edition_dbt_transforms/models/custom_metrics/base/base_pipeline.sql
+++
@@ -1,4 +0,0 @@
-select
-    "id" as id,
-    "name" as name
-from "faros"."public"."cicd_Pipeline"
--- dbt-transforms/target/run/faros_community_edition_dbt_transforms/models/custom_metrics/base/base_pipeline.sql
+++
@@ -1,7 +0,0 @@
-
-  create view "faros"."public"."base_pipeline__dbt_tmp" as (
-    select
-    "id" as id,
-    "name" as name
-from "faros"."public"."cicd_Pipeline"
-  );

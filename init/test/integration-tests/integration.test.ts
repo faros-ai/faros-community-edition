cb5c3b0317d9284775dcb21928fe1f0fadfc7d9a
Yandry Perez Clemente <99700024+ypc-faros@users.noreply.github.com>
2022-05-10 18:12:54 -0400

    Add event cli integration test

--- init/test/integration-tests/integration.test.ts
+++ init/test/integration-tests/integration.test.ts
@@ -65,6 +65,28 @@ describe('integration tests', () => {
     60 * 1000
   );
 
+  test(
+    'check events cli writes in Hasura',
+    async () => {
+      const origin = 'Faros_Script_Event';
+
+      sendCIEvent(origin);
+
+      expect(await hasuraClient.getCicdBuildCount(origin)).toBe(1);
+      expect(await hasuraClient.getCicdPipelineCount(origin)).toBe(1);
+      expect(await hasuraClient.getCicdOrganizationCount(origin)).toBe(2);
+      expect(await hasuraClient.getCicdArtifactCount(origin)).toBe(1);
+      expect(
+        await hasuraClient.getCicdArtifactCommitAssociationCount(origin)
+      ).toBe(1);
+      expect(await hasuraClient.getCicdRepositoryCount(origin)).toBe(1);
+      expect(
+        await hasuraClient.getVcsPullRequestCommitAssociationCount(origin)
+      ).toBe(1);
+    },
+    60 * 1000
+  );
+
   function writeRecords(tmpDir: string) {
     execSync(`docker pull farosai/airbyte-faros-destination \
     && cat ${tmpDir}/streams.in \
@@ -73,5 +95,21 @@ describe('integration tests', () => {
     farosai/airbyte-faros-destination write \
     --catalog /integration-test/catalog.json \
     --config /integration-test/config.json`);
+  }
+
+  function sendCIEvent(origin: string) {
+    execSync(`docker pull farosai/faros-events-cli:latest \
+    && docker run -i --network host farosai/faros-events-cli:latest CI \
+    --run "<run_source>://<run_organization>/<run_pipeline>/<run_id>" \
+    --commit "<commit_source>://<commit_organization>/<commit_repository>/<commit_sha>" \
+    --pull_request_number 1 \
+    --artifact "<artifact_source>://<artifact_organization>/<artifact_repository>/<artifact_id>" \
+    --run_status "Success" \
+    --run_status_details "Some extra details" \
+    --run_start_time "1000" \
+    --run_end_time "2000" \
+    --hasura_admin_secret ${hasuraAdminSecret} \
+    --origin ${origin} \
+    --community_edition`);
   }
 });

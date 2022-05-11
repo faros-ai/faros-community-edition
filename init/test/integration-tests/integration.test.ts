0e8b738820d99ab08744ce51769a3ef93c5b58d2
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2022-05-11 12:24:52 -0400

    Add CD event test (#154)
    Co-authored-by: Matthew Tovbin <tovbinm@users.noreply.github.com>

--- init/test/integration-tests/integration.test.ts
+++ init/test/integration-tests/integration.test.ts
@@ -87,6 +87,23 @@ describe('integration tests', () => {
     60 * 1000
   );
 
+  test(
+    'check CD event writes in Hasura',
+    async () => {
+      const origin = 'CD_test';
+
+      sendCDEvent(origin);
+
+      expect(await hasuraClient.getComputeApplicationCount(origin)).toBe(1);
+      expect(await hasuraClient.getCicdArtifactDeploymentCount(origin)).toBe(1);
+      expect(await hasuraClient.getCicdDeploymentCount(origin)).toBe(1);
+      expect(await hasuraClient.getCicdBuildCount(origin)).toBe(1);
+      expect(await hasuraClient.getCicdPipelineCount(origin)).toBe(1);
+      expect(await hasuraClient.getCicdOrganizationCount(origin)).toBe(1);
+    },
+    60 * 1000
+  );
+
   function writeRecords(tmpDir: string) {
     execSync(`docker pull farosai/airbyte-faros-destination \
     && cat ${tmpDir}/streams.in \
@@ -108,6 +125,24 @@ describe('integration tests', () => {
     --run_status_details "Some extra details" \
     --run_start_time "1000" \
     --run_end_time "2000" \
+    --hasura_admin_secret ${hasuraAdminSecret} \
+    --origin ${origin} \
+    --community_edition`);
+  }
+
+  function sendCDEvent(origin: string) {
+    execSync(`docker pull farosai/faros-events-cli:latest \
+    && docker run -i --network host farosai/faros-events-cli:latest CD \
+    --run "GitHub://soraf-ai/faros-community-edition/123" \
+    --artifact "GitHub://soraf-ai/faros-community-edition/456" \
+    --run_status "Success" \
+    --run_status_details "Some extra details" \
+    --run_start_time "1000" \
+    --run_end_time "2000" \
+    --deploy "GitHub://faros/prod/789" \
+    --deploy_status "Success" \
+    --deploy_start_time "3000" \
+    --deploy_end_time "4000" \
     --hasura_admin_secret ${hasuraAdminSecret} \
     --origin ${origin} \
     --community_edition`);

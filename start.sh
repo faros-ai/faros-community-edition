964ac3337b11800283e091ac01eb1b6e8b1a9854
Thomas Gerber <thomas@faros.ai>
2022-07-22 12:03:58 -0700

    Airbyte 39 (#203)
    * Upgrade to Airbyte 39
    
    Co-authored-by: Chris Wu <chris@faros.ai>

--- start.sh
+++ start.sh
@@ -34,8 +34,8 @@ export FAROS_INIT_IMAGE=farosai.docker.scarf.sh/farosai/faros-ce-init:latest
 docker-compose pull faros-init
 
 if [[ $(uname -m 2> /dev/null) == 'arm64' ]]; then
-    # Use Airbyte and Metabase images built for Apple M1
-    AIRBYTE_IMAGE_PREFIX="farosai.docker.scarf.sh/farosai/airbyte-" METABASE_IMAGE="farosai.docker.scarf.sh/farosai/metabase-m1" \
+    # Use Metabase images built for Apple M1
+    METABASE_IMAGE="farosai.docker.scarf.sh/farosai/metabase-m1" \
     docker-compose up --build --remove-orphans
 else
     docker-compose up --build --remove-orphans

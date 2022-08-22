16c3ccdf26c8b39fc9e32614de199854fcdf2122
Brad Reed 😎 <brad@bradreed.co.uk>
2022-08-22 15:30:55 +0100

    add elephant_TeamRepositoryAssociation table

--- canonical-schema/V010__elephant_team_model.sql
+++ canonical-schema/V010__elephant_team_model.sql
@@ -1,13 +1,9 @@
-create table "elephant_Team" (
-  id text generated always as (pkey(source, uid)) stored primary key,
+create table "elephant_TeamRepositoryAssociation" (
+  id text generated always as (pkey(team, repository)) stored primary key,
   origin text,
   "refreshedAt" timestamptz not null default now(),
-  uid text not null,
-  name text not null,
-  slug text not null,
-  source text,
-  organization text
+  team text not null,
+  repository text not null
 );
-
-alter table "elephant_Team" add foreign key (organization) references "vcs_Organization"(id);
-
+alter table "elephant_TeamRepositoryAssociation" add foreign key (team) references "ims_Team"(id);
+alter table "elephant_TeamRepositoryAssociation" add foreign key (repository) references "vcs_Repository"(id);

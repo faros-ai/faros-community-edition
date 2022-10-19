750939cf5bb946566e8c175d5b27ff669eb92bd8
ypc-faros <99700024+ypc-faros@users.noreply.github.com>
2022-10-18 22:39:09 -0400

    Add back pull of latest init image (#230)

--- start.sh
+++ start.sh
@@ -48,6 +48,7 @@ main() {
 
   # Ensure we're using the latest faros-init image
   export FAROS_INIT_IMAGE=farosai.docker.scarf.sh/farosai/faros-ce-init:latest
+  docker compose pull faros-init
 
   if [[ $(uname -m 2> /dev/null) == 'arm64' ]]; then
       # Use Metabase images built for Apple M1

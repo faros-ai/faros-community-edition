d1094a66e8c79523de315e3d5c383336999c1c4d
Thomas Gerber <thomas@faros.ai>
2022-10-25 10:51:28 -0700

    Adds support for GitLab

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -1,8 +1,9 @@
 import {program} from 'commander';
 
 import {Airbyte} from './airbyte/airbyte-client';
-import {makeBitbucketCommand, runBitbucket} from './bitbucket/run';
 import {makeGithubCommand, runGithub} from './github/run';
+import {makeGitlabCommand, runGitlab} from './gitlab/run';
+import {makeBitbucketCommand, runBitbucket} from './bitbucket/run';
 import {makeJiraCommand, runJira} from './jira/run';
 import {display, terminalLink} from './utils';
 import {runSelect} from './utils/prompts';
@@ -11,8 +12,9 @@ const DEFAULT_AIRBYTE_URL = 'http://localhost:8000';
 const DEFAULT_METABASE_URL = 'http://localhost:3000';
 
 export async function main(): Promise<void> {
-  program.addCommand(makeBitbucketCommand());
   program.addCommand(makeGithubCommand());
+  program.addCommand(makeGitlabCommand());
+  program.addCommand(makeBitbucketCommand());
   program.addCommand(makeJiraCommand());
 
   // Commander doesn't allow for empty subcommand names, even if the subcommand
@@ -27,16 +29,19 @@ export async function main(): Promise<void> {
         const source = await runSelect({
           name: 'source',
           message: 'Select a source',
-          choices: ['bitbucket', 'github', 'jira', 'I\'m done!'],
+          choices: ['GitHub', 'GitLab', 'Bitbucket', 'Jira', 'I\'m done!'],
         });
         switch (source) {
-          case 'bitbucket':
-            await runBitbucket({airbyte});
-            break;
-          case 'github':
+          case 'GitHub':
             await runGithub({airbyte});
             break;
-          case 'jira':
+          case 'GitLab':
+            await runGitlab({airbyte});
+            break;
+          case 'Bitbucket':
+            await runBitbucket({airbyte});
+            break;
+          case 'Jira':
             await runJira({airbyte});
             break;
           case 'I\'m done!':

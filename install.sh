81572bca1e9aba24afb177a46a72db42a48eee4f
Thomas Gerber <thomas@faros.ai>
2022-11-17 15:48:39 -0800

    Ignores git clone errors in install script (#256)
    * Ignores git clone errors and re-pull

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/faros-ai/faros-community-edition.git
-cd faros-community-edition || exit
+git clone https://github.com/faros-ai/faros-community-edition.git || true
+cd faros-community-edition && git pull || exit
 ./start.sh --run-cli "$@"

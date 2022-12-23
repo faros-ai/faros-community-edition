b6b40aad88faa44c977e68d38365d2b13371553e
Thomas Gerber <thomas@faros.ai>
2022-12-22 20:36:51 -0800

    shutdown before start to avoid race conditions (#260)
    * More aggressive healthcheck
    
    * Exit if start called on running instance
    If Faros CE is running when one launches start, the script exits. 
    This ensure a consistent interaction between services on start
    (e.g. metabase container does not start until db is configured)
    
    * New stop.sh script
    Note that `docker compose down` command will only stop services
    listed in the compose file of the folder

--- start.sh
+++ start.sh
@@ -44,6 +44,13 @@ function parseFlags() {
 }
 
 main() {
+    RUNNING=$(docker compose ps -q --status=running | wc -l)
+    if [ "$RUNNING" -gt 0 ]; then
+        printf "Faros CE is still running. \n"
+        printf "You can stop it with the stop.sh command. \n"
+        exit 1
+    fi
+
     parseFlags "$@"
     EMAIL_FILE=".faros-email"
     if [[ -n "$email_override" ]]; then
@@ -86,7 +93,7 @@ main() {
 
     if ((run_cli)); then
         CONTAINER_EXIT_CODE=$(docker wait faros-community-edition-faros-init-1)
-        if [ "$CONTAINER_EXIT_CODE" != 0 ]; then
+        if [ "$CONTAINER_EXIT_CODE" -gt 0 ]; then
             printf "An error occured during the initialization of Faros CE.\n"
             printf "For troubleshooting help, you can bring this log on our Slack workspace:\n"
             printf "https://community.faros.ai/docs/slack \n"
---
+++ stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# only services in the compose file will be stopped
+docker compose down
+exit
